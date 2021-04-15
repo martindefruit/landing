0b2cc306314fc50149b5de292955294710f93d5c
Martin de Frutos <martindfru@gmail.com>
2021-04-15 11:09:33 +0200

    Initial commit for the landing page

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <b-jumbotron class="bg-image header negative-m">
+
+
+  </b-jumbotron>
+</div>
+</template>
+
+<style>
+  .header {
+    height: 600px;
+  }
+  .negative-m {
+    margin-top: -75px;
+  }
+  .bg-image {
+    background-image: url(~/assets/landing.gif);
+  }
+
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <b-navbar toggleable="lg">
+    <b-navbar-brand href="#"><Logo /></b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<style>
+</style>
